3898d3484a7cc2f7951f5abe638b43a14d62df12
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-01-01 19:45:37 +0100

    WIP

---
+++ src/ProcEngine/Win2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+using LearnOpenTK.Common;
+using ProcEngine;
+using System.IO;
+using System.Collections.Generic;
+
+namespace LearnOpenTK
+{
+    public class Win2 : GameWindow
+    {
+
+        public Win2(int width, int height, string title) : base(width, height, GraphicsMode.Default, title, GameWindowFlags.Default, DisplayDevice.Default, 4, 0, GraphicsContextFlags.Default) { }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.Enable(EnableCap.DepthTest);
+
+            //Shader shader = new Shader("", "");
+            //Shader simpleDepthShader = new Shader("", "");
+
+            // set up vertex data (and buffer(s)) and configure vertex attributes
+            // ------------------------------------------------------------------
+            float[] planeVertices = new float[]{
+        // positions            // normals         // texcoords
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+         25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+        -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+         25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+    };
+
+            int planeVBO;
+            int planeVAO;
+            GL.GenVertexArrays(1, out planeVAO);
+            GL.GenBuffers(1, out planeVBO);
+            GL.BindVertexArray(planeVAO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, planeVBO);
+            GL.BufferData(BufferTarget.ArrayBuffer, planeVertices.Length, planeVertices, BufferUsageHint.StaticDraw);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(2);
+            GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
+            GL.BindVertexArray(0);
+        }
+
+    }
+
+}
