7c0567ee5fbb8e41e140200323369714d62eb9a8
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-01-02 22:56:58 +0100

    added tutorial sample

--- src/ProcEngine/Win2.cs
+++ src/ProcEngine/Win2.cs
@@ -13,14 +13,29 @@ namespace LearnOpenTK
     public class Win2 : GameWindow
     {
 
+        Vector3 lightPos;
+        Shader shader;
+        Shader simpleDepthShader;
+        int SHADOW_WIDTH;
+        int SHADOW_HEIGHT;
+        int depthMapFBO;
+        int woodTexture;
+        Camera camera;
+        int depthMap;
+        int planeVAO;
+        int cubeVAO;
+        int cubeVBO;
+
         public Win2(int width, int height, string title) : base(width, height, GraphicsMode.Default, title, GameWindowFlags.Default, DisplayDevice.Default, 4, 0, GraphicsContextFlags.Default) { }
 
         protected override void OnLoad(EventArgs e)
         {
             GL.Enable(EnableCap.DepthTest);
 
-            //Shader shader = new Shader("", "");
-            //Shader simpleDepthShader = new Shader("", "");
+            camera = new Camera(new Vector3(0.0f, 0.0f, 3.0f), (float)800 / 600);
+
+            shader = new Shader("Shaders/3.1.3.shadow_mapping/3.1.3.shadow_mapping.vs", "Shaders/3.1.3.shadow_mapping/3.1.3.shadow_mapping.fs");
+            simpleDepthShader = new Shader("Shaders/3.1.3.shadow_mapping/3.1.3.debug_quad.vs", "Shaders/3.1.3.shadow_mapping/3.1.3.debug_quad_depth.fs");
 
             // set up vertex data (and buffer(s)) and configure vertex attributes
             // ------------------------------------------------------------------
@@ -35,13 +50,13 @@ namespace LearnOpenTK
          25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
     };
 
+            // plane VAO
             int planeVBO;
-            int planeVAO;
             GL.GenVertexArrays(1, out planeVAO);
             GL.GenBuffers(1, out planeVBO);
             GL.BindVertexArray(planeVAO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, planeVBO);
-            GL.BufferData(BufferTarget.ArrayBuffer, planeVertices.Length, planeVertices, BufferUsageHint.StaticDraw);
+            GL.BufferData(BufferTarget.ArrayBuffer, planeVertices.Length * sizeof(float), planeVertices, BufferUsageHint.StaticDraw);
             GL.EnableVertexAttribArray(0);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
             GL.EnableVertexAttribArray(1);
@@ -49,7 +64,217 @@ namespace LearnOpenTK
             GL.EnableVertexAttribArray(2);
             GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
             GL.BindVertexArray(0);
+
+            var woodTexturetxt = new Texture("Ressources/wood.png");
+            woodTexture = woodTexturetxt.Handle;
+
+            // configure depth map FBO
+            // -----------------------
+            SHADOW_WIDTH = 1024;
+            SHADOW_HEIGHT = 1024;
+            GL.GenFramebuffers(1, out depthMapFBO);
+            // create depth texture
+            GL.GenTextures(1, out depthMap);
+            GL.BindTexture(TextureTarget.Texture2D, depthMap);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.DepthComponent, SHADOW_WIDTH, SHADOW_HEIGHT, 0, PixelFormat.DepthComponent, PixelType.Float, IntPtr.Zero);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)All.ClampToBorder);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)All.ClampToBorder);
+            float[] borderColor = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureBorderColor, borderColor);
+            // attach depth texture as FBO's depth buffer
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, depthMapFBO);
+            GL.FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthAttachment, TextureTarget.Texture2D, depthMap, 0);
+            GL.DrawBuffer(DrawBufferMode.None);
+            GL.ReadBuffer(ReadBufferMode.None);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+
+            // shader configuration
+            // --------------------
+            shader.Use();
+            shader.SetInt("diffuseTexture", 0);
+            shader.SetInt("shadowMap", 1);
+            //debugDepthQuad.use();
+            //debugDepthQuad.setInt("depthMap", 0);
+
+            // lighting info
+            // -------------
+            lightPos = new Vector3(-2.0f, 4.0f, -1.0f);
         }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            // input
+            // -----
+            //processInput(window);
+
+            // change light position over time
+            //lightPos.x = sin(glfwGetTime()) * 3.0f;
+            //lightPos.z = cos(glfwGetTime()) * 2.0f;
+            //lightPos.y = 5.0 + cos(glfwGetTime()) * 1.0f;
+
+            // render
+            // ------
+            GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // 1. render depth of scene to texture (from light's perspective)
+            // --------------------------------------------------------------
+            Matrix4 lightProjection; Matrix4 lightView;
+            Matrix4 lightSpaceMatrix;
+            float near_plane = 1.0f, far_plane = 7.5f;
+            //lightProjection = glm::perspective(glm::radians(45.0f), (GLfloat)SHADOW_WIDTH / (GLfloat)SHADOW_HEIGHT, near_plane, far_plane); // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
+            lightProjection = Matrix4.CreateOrthographicOffCenter(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
+            lightView = Matrix4.LookAt(lightPos, new Vector3(0.0f), new Vector3(0.0f, 1.0f, 0.0f));
+            lightSpaceMatrix = lightProjection * lightView;
+            // render scene from light's point of view
+            simpleDepthShader.Use();
+            simpleDepthShader.SetMatrix4("lightSpaceMatrix", lightSpaceMatrix);
+
+            GL.Viewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, depthMapFBO);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, woodTexture);
+            renderScene(simpleDepthShader);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+
+            // reset viewport
+            GL.Viewport(0, 0, 800, 600);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // 2. render scene as normal using the generated depth/shadow map  
+            // --------------------------------------------------------------
+            GL.Viewport(0, 0, 800, 600);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            shader.Use();
+            Matrix4 projection = camera.GetProjectionMatrix(); // MOD
+            Matrix4 view = camera.GetViewMatrix();
+            shader.SetMatrix4("projection", projection);
+            shader.SetMatrix4("view", view);
+            // set light uniforms
+            shader.SetVector3("viewPos", camera.Position);
+            shader.SetVector3("lightPos", lightPos);
+            shader.SetMatrix4("lightSpaceMatrix", lightSpaceMatrix);
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, woodTexture);
+            GL.ActiveTexture(TextureUnit.Texture1);
+            GL.BindTexture(TextureTarget.Texture2D, depthMap);
+            renderScene(shader);
+
+            // render Depth map to quad for visual debugging
+            // ---------------------------------------------
+            //debugDepthQuad.use();
+            //debugDepthQuad.setFloat("near_plane", near_plane);
+            //debugDepthQuad.setFloat("far_plane", far_plane);
+            //glActiveTexture(GL_TEXTURE0);
+            //glBindTexture(GL_TEXTURE_2D, depthMap);
+            //renderQuad();
+
+            // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+            // -------------------------------------------------------------------------------
+            SwapBuffers();
+        }
+
+        private void renderScene(Shader shader)
+        {
+            // floor
+            Matrix4 model = Matrix4.Identity;
+            shader.SetMatrix4("model", model);
+            GL.BindVertexArray(planeVAO);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
+            // cubes
+            model = Matrix4.Identity;
+            model *= Matrix4.CreateTranslation(new Vector3(0.0f, 1.5f, 0.0f));
+            model *= Matrix4.CreateScale(new Vector3(0.5f));
+            shader.SetMatrix4("model", model);
+            renderCube();
+            model = Matrix4.Identity;
+            model *= Matrix4.CreateTranslation(new Vector3(2.0f, 0.0f, 1.0f));
+            model *= Matrix4.CreateScale(new Vector3(0.5f));
+            shader.SetMatrix4("model", model);
+            renderCube();
+            model = Matrix4.Identity;
+            model *= Matrix4.CreateTranslation(new Vector3(-1.0f, 0.0f, 2.0f));
+            //model = glm::rotate(model, glm::radians(60.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0)));
+            model *= Matrix4.CreateScale(new Vector3(0.25f));
+            shader.SetMatrix4("model", model);
+            renderCube();
+        }
+
+        public void renderCube()
+        {
+            // initialize (if necessary)
+            if (cubeVAO == 0)
+            {
+                float[] vertices = new float[] {
+            // back face
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+             1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+             1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
+            -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+            -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
+            // front face
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+             1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
+             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+             1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
+            -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
+            -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
+            // left face
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            -1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
+            // right face
+             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+             1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right         
+             1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
+             1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
+             1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left     
+            // bottom face
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+             1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
+             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+             1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
+            -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
+            -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
+            // top face
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+             1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+             1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right     
+             1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
+            -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
+            -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left        
+        };
+                GL.GenVertexArrays(1, out cubeVAO);
+                GL.GenBuffers(1, out cubeVBO);
+                // fill buffer
+                GL.BindBuffer(BufferTarget.ArrayBuffer, cubeVBO);
+                GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+                // link vertex attributes
+                GL.BindVertexArray(cubeVAO);
+                GL.EnableVertexAttribArray(0);
+                GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
+                GL.EnableVertexAttribArray(1);
+                GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (3 * sizeof(float)));
+                GL.EnableVertexAttribArray(2);
+                GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), (6 * sizeof(float)));
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                GL.BindVertexArray(0);
+            }
+            // render Cube
+            GL.BindVertexArray(cubeVAO);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+            GL.BindVertexArray(0);
+
+        }
+
 
     }
 
