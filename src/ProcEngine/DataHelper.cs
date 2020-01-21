377c366632ce740555b9c26599844ef76b383db7
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-01-21 20:00:50 +0100

    clean up

--- src/ProcEngine/DataHelper.cs
+++ src/ProcEngine/DataHelper.cs
@@ -1,5 +1,4 @@
 ﻿using OpenTK;
-using OpenTK.Graphics.OpenGL4;
 using System;
 using System.Drawing;
 using System.Linq;
--- src/ProcEngine/Program.cs
+++ src/ProcEngine/Program.cs
@@ -12,9 +12,7 @@ namespace ProcEngine
                 Backend = PlatformBackend.PreferX11,
             });
 
-            //var demo = new TDemoWindow();
-            var demo = new Window(800, 600, "blubb");
-            //var demo = new Win2(800, 600, "blubb");
+            var demo = new Window(800, 600, "ProcEngine");
 
             // Run the game at 60 updates per second
             demo.Run(60.0);
