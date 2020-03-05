3059965d5b9acef63b26b006b9af48a4985b31df
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-03-05 19:25:37 +0100

    load image from bitmap

--- src/AxEngine/OpenGL/Texture.cs
+++ src/AxEngine/OpenGL/Texture.cs
@@ -177,8 +177,23 @@ namespace AxEngine
             GL.TexParameter(Target, TextureParameterName.TextureWrapR, (int)All.ClampToEdge);
         }
 
-        // Create texture from path.
         public Texture(string path)
+        {
+            using (var image = LoadBitmap(path))
+            {
+                InitFromBitmap(image);
+            }
+            Console.WriteLine($"Loaded Texture #{Handle} {path}");
+            ObjectLabel = Path.GetFileName(path);
+        }
+
+        // Create texture from path.
+        public Texture(Bitmap image)
+        {
+            InitFromBitmap(image);
+        }
+
+        private void InitFromBitmap(Bitmap image)
         {
             Target = TextureTarget.Texture2D;
             // Generate handle
@@ -186,47 +201,43 @@ namespace AxEngine
 
             // Bind the handle
             Use();
-            ObjectLabel = Path.GetFileName(path);
 
             // For this example, we're going to use .NET's built-in System.Drawing library to load textures.
 
             // Load the image
-            using (var image = LoadBitmap(path))
-            {
-                // First, we get our pixels from the bitmap we loaded.
-                // Arguments:
-                //   The pixel area we want. Typically, you want to leave it as (0,0) to (width,height), but you can
-                //   use other rectangles to get segments of textures, useful for things such as spritesheets.
-                //   The locking mode. Basically, how you want to use the pixels. Since we're passing them to OpenGL,
-                //   we only need ReadOnly.
-                //   Next is the pixel format we want our pixels to be in. In this case, ARGB will suffice.
-                //   We have to fully qualify the name because OpenTK also has an enum named PixelFormat.
-                var data = image.LockBits(
-                    new Rectangle(0, 0, image.Width, image.Height),
-                    ImageLockMode.ReadOnly,
-                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            // First, we get our pixels from the bitmap we loaded.
+            // Arguments:
+            //   The pixel area we want. Typically, you want to leave it as (0,0) to (width,height), but you can
+            //   use other rectangles to get segments of textures, useful for things such as spritesheets.
+            //   The locking mode. Basically, how you want to use the pixels. Since we're passing them to OpenGL,
+            //   we only need ReadOnly.
+            //   Next is the pixel format we want our pixels to be in. In this case, ARGB will suffice.
+            //   We have to fully qualify the name because OpenTK also has an enum named PixelFormat.
+            var data = image.LockBits(
+                new Rectangle(0, 0, image.Width, image.Height),
+                ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
-                // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D
-                // Arguments:
-                //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
-                //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
-                //   Target format of the pixels. This is the format OpenGL will store our image with.
-                //   Width of the image
-                //   Height of the image.
-                //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
-                //   The format of the pixels, explained above. Since we loaded the pixels as ARGB earlier, we need to use BGRA.
-                //   Data type of the pixels.
-                //   And finally, the actual pixels.
-                GL.TexImage2D(Target,
-                    0,
-                    PixelInternalFormat.Rgba,
-                    image.Width,
-                    image.Height,
-                    0,
-                    PixelFormat.Bgra,
-                    PixelType.UnsignedByte,
-                    data.Scan0);
-            }
+            // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D
+            // Arguments:
+            //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
+            //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
+            //   Target format of the pixels. This is the format OpenGL will store our image with.
+            //   Width of the image
+            //   Height of the image.
+            //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
+            //   The format of the pixels, explained above. Since we loaded the pixels as ARGB earlier, we need to use BGRA.
+            //   Data type of the pixels.
+            //   And finally, the actual pixels.
+            GL.TexImage2D(Target,
+                0,
+                PixelInternalFormat.Rgba,
+                image.Width,
+                image.Height,
+                0,
+                PixelFormat.Bgra,
+                PixelType.UnsignedByte,
+                data.Scan0);
 
             // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
 
@@ -250,8 +261,6 @@ namespace AxEngine
             // OpenGL will automatically switch between mipmaps when an object gets sufficiently far away.
             // This prevents distant objects from having their colors become muddy, as well as saving on memory.
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
-
-            Console.WriteLine($"Loaded Texture #{Handle} {path}");
         }
 
         private static Bitmap LoadBitmap(string path)
