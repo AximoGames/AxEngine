65488333ef9bded0e3a2ddfa4510c19c50e90892
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-03-27 12:48:53 +0100

    tests WIP

--- AxTests/Tests.cs
+++ AxTests/Tests.cs
@@ -1,17 +1,54 @@
-﻿using System;
+﻿// This file is part of Aximo, a Game Engine written in C#. Web: https://github.com/AximoGames
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Drawing;
+using Aximo.Engine;
+using Aximo.Render;
+using OpenTK;
 using Xunit;
 
-namespace test
+namespace Aximo.AxDemo
 {
 
-    public class Tester
+    public class Tester : RenderApplicationTests
     {
+
+        public Tester() : base(new RenderApplicationStartup
+        {
+            WindowTitle = "AxEngineDemo",
+            WindowSize = new Vector2i(800, 600),
+            WindowBorder = WindowBorder.Fixed,
+        })
+        {
+
+        }
 
         [Fact]
         public void test()
         {
-            Assert.Null(null);
+
+            var mat = new GameMaterial()
+            {
+                DiffuseTexture = GameTexture.GetFromFile("Textures/woodenbox.png"),
+                SpecularTexture = GameTexture.GetFromFile("Textures/woodenbox_specular.png"),
+                Ambient = 1f,
+                ColorBlendMode = MaterialColorBlendMode.Set,
+                Color = new Vector3(0, 1, 0),
+                PipelineType = PipelineType.Forward,
+            };
+
+            GameContext.AddActor(new Actor(new DebugCubeComponent()
+            {
+                Name = "Box1",
+                RelativeRotation = new Vector3(0, 0, 0.5f).ToQuaternion(),
+                RelativeScale = new Vector3(1),
+                RelativeTranslation = new Vector3(0, 0, 0.5f),
+                Material = mat,
+            }));
+            while (true) { }
         }
+
     }
 
 }
