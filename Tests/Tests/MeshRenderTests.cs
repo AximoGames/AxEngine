4d94521e7b6e9db45c27f4d1cb3b88dd0a01093d
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-05-02 15:42:44 +0200

    addes sphere test

---
+++ Tests/Tests/MeshRenderTests.cs
@@ -0,0 +1,48 @@
+﻿// This file is part of Aximo, a Game Engine written in C#. Web: https://github.com/AximoGames
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Aximo.Engine;
+using Aximo.Render;
+using OpenToolkit;
+using OpenToolkit.Mathematics;
+using Xunit;
+
+namespace Aximo.AxTests
+{
+    public class MeshRenderTests : TestBase
+    {
+        public MeshRenderTests()
+        {
+        }
+
+        [Fact]
+        public void Box()
+        {
+            GameContext.AddActor(new Actor(new DirectionalLightComponent()
+            {
+                Name = "StaticLight",
+                RelativeTranslation = new Vector3(-0.2f, -2.1f, 1.85f),
+            }));
+
+            GameMaterial material = new GameMaterial
+            {
+                DiffuseTexture = GameTexture.GetFromFile("Textures/woodenbox.png"),
+                SpecularTexture = GameTexture.GetFromFile("Textures/woodenbox_specular.png"),
+                Ambient = 0.5f,
+                PipelineType = PipelineType.Deferred,
+            };
+
+            GameContext.AddActor(new Actor(new SphereComponent()
+            {
+                Name = "Sphere1",
+                Transform = GetTestTransform(),
+                Material = material,
+            }));
+
+            RenderAndCompare(nameof(Box));
+        }
+
+    }
+}
