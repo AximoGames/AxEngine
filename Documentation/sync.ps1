4d6c305e7e12ebdc2ff828a016f3f27ba6af3e2f
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-05-21 22:14:01 +0200

    moved documentation

--- Documentation/sync.ps1
+++ Documentation/sync.ps1
@@ -4,13 +4,13 @@ $dir = Split-Path $scriptpath
 Push-Location $dir
 
 $sourceRoot = "_site"
-$destinationRoot = "..\..\..\AximoGames-pages\docs\"
+$destinationRoot = "..\..\AximoGames-pages\docs\"
 
 Remove-Item -Recurse -Force $destinationRoot
 Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
 
-$sourceRoot = "..\..\..\AximoGames-pages\docs-base\*"
-$destinationRoot = "..\..\..\AximoGames-pages\docs\"
+$sourceRoot = "..\..\AximoGames-pages\docs-base\*"
+$destinationRoot = "..\..\AximoGames-pages\docs\"
 
 Copy-Item -Path $sourceRoot -Recurse -Destination $destinationRoot -Container
 
