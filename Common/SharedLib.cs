7236ccc2b166310580f7ff98e74e4ca95d67f39c
Sebastian Loncar <sebastian.loncar@gmail.com>
2020-07-11 14:32:18 +0200

    fix unix font

--- Common/SharedLib.cs
+++ Common/SharedLib.cs
@@ -1,6 +1,7 @@
 ﻿// This file is part of Aximo, a Game Engine written in C#. Web: https://github.com/AximoGames
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Globalization;
 using SixLabors.Fonts;
 
@@ -9,6 +10,13 @@ namespace Aximo
     public static class SharedLib
     {
         public static CultureInfo LocaleInvariant = CultureInfo.InvariantCulture;
-        public static FontFamily DefaultFontFamily() => SystemFonts.Find("Arial");
+        public static FontFamily DefaultFontFamily()
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                return SystemFonts.Find("Arial");
+            else
+                return SystemFonts.Find("DejaVu Sans");
+
+        }
     }
 }
